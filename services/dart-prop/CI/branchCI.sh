d12b1c587aee8ab5fee9b0227fb6576f86ec8d28
Andrew Porter <andrew@mbay.io>
2019-01-10 16:45:00 -0800

    Formatting tweak on output

--- services/dart-prop/CI/branchCI.sh
+++ services/dart-prop/CI/branchCI.sh
@@ -3,6 +3,7 @@ echo "[INFO] -> Running actions for branch $SERVICE..."
 # Get dependencies, fail shell on bad exit
 pub get
 failOnBadExit $?
+echo
 
 # Set bulid platform
 DART_BUILD_PLATFORM="vm"
@@ -13,6 +14,7 @@ pub run test -p $DART_BUILD_PLATFORM --reporter expanded |
 # pub run build_runner test --fail-on-severe -- -p $DART_BUILD_PLATFORM --reporter expanded |
 tee $BASE"/services/artifacts/"$SERVICE"_branch_results.out"
 failOnBadExit $?
+echo
 
 # All done!
 echo "[INFO] -> Finished!"
